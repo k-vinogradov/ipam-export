174e6ad58a78f676066e5e0ce593fca38eca7d27
Konstantin <mail@k-vinogradov.ru>
2016-06-07 17:00:13 +0700

    First commit

---
+++ api.py
@@ -0,0 +1,101 @@
+import requests
+
+from datetime import datetime
+from json.decoder import JSONDecodeError
+from requests.auth import HTTPBasicAuth
+
+
+class APIException(BaseException):
+    def __init__(self, value, code=None):
+        super(BaseException, self).__init__()
+        self._value = value
+        self.code = int(code)
+
+    def __str__(self):
+        return 'IPAM API Error: {0} ({1})'.format(self._value, self.code)
+
+
+class API(object):
+    def __init__(self, url, app, username, password):
+        self._url = url + '/' + app + '/'
+        auth = HTTPBasicAuth(username, password)
+        response = requests.post(self._url + 'user/', auth=auth)
+        try:
+            json_response = response.json()
+            if 'success' in json_response:
+                if json_response['success']:
+                    self._token = json_response['data']['token']
+                    self._expires = datetime.strptime(json_response['data']['expires'], '%Y-%m-%d %H:%M:%S')
+                else:
+                    raise APIException(datetime['message'])
+            else:
+                raise APIException('Unknown error')
+        except JSONDecodeError:
+            raise APIException(response.content)
+
+    def _request(self, request_type, path, params=None):
+        url = self._url + path + '/'
+        if params:
+            url += '&'
+            for key, value in params.items():
+                url += '{0}={1}'.format(key, value)
+        headers = {'phpipam-token': self._token}
+        response = request_type(url, headers=headers)
+        try:
+            json_response = response.json()
+            if 'success' in json_response:
+                if json_response['success']:
+                    return json_response['data']
+                else:
+                    raise APIException(json_response['message'], json_response['code'])
+            else:
+                raise APIException('Unknown error')
+        except JSONDecodeError:
+            raise APIException(response.content)
+
+    def _get_request(self, path, **kwargs):
+        return self._request(requests.get, path, params=kwargs)
+
+    def _options_request(self, path, **kwargs):
+        return self._request(requests.options, path, params=kwargs)
+
+    def filter_sections(self, **kwargs):
+        data = self._get_request('sections', **kwargs)
+        if type(data) == dict:
+            return list(data.values())
+        else:
+            return data
+
+    def get_section(self, sid):
+        return self._get_request('sections/{}'.format(sid))
+
+    def get_subnets(self, sid, **kwargs):
+        section = self.get_section(sid)
+        sid = section['id']
+        path = 'sections/{}/subnets/'.format(sid)
+        data = self._get_request(path)
+        result = []
+        for subnet in data:
+            append = True
+            for key, value in kwargs.items():
+                if key in subnet:
+                    if str(value) != subnet[key]:
+                        append = False
+                else:
+                    APIException('Invalid filter value: "{}"'.format(key))
+                if not append:
+                    break
+            if append:
+                result.append(subnet)
+        return result
+
+    def get_subnet(self, subnet, section=None, **kwargs):
+        if section:
+            sid = self.get_section(section)['id']
+            kwargs['sectionId'] = sid
+        return self._get_request('subnets/{}'.format(subnet), **kwargs)
+
+    def get_addresses(self, subnet_id):
+        subnet_data = self.get_subnet(subnet_id)
+        subnet_id = subnet_data['id']
+        return self._get_request('/subnets/{}/addresses/'.format(subnet_id))
---
+++ ptr_export.py
@@ -0,0 +1,112 @@
+from api import API, APIException
+from configparser import ConfigParser
+from ipcalc import IP, Network
+from jinja2 import Template
+from hashlib import sha256
+
+parser = ConfigParser()
+parser.read('ipam.conf')
+
+url = parser['api']['url']
+app = parser['api']['app']
+username = parser['api']['username']
+password = parser['api']['password']
+var_dir = parser['general']['var_dir']
+
+api = API(url, app, username, password)
+
+resources = {}
+zones = {}
+
+for zone in parser.sections():
+    if zone[:5] == 'zone ':
+        section = parser[zone]['section']
+        if section not in resources:
+            resources[section] = []
+        zones[zone] = dict(parser[zone])
+        zones[zone]['ns'] = zones[zone]['ns'].split(' ')
+        zones[zone]['prefix'] = Network(zones[zone]['prefix'])
+        zones[zone]['admin_mail'] = zones[zone]['admin_mail'].replace('@', '.')
+
+for section_title in resources:
+    subnets = {}
+    hosts = {}
+    resources[section_title] = []
+    for subnet_data in api.get_subnets(section_title):
+        try:
+            if subnet_data['isFull'] == '1':
+                ip = subnet_data['subnet'] + '/' + subnet_data['mask']
+                domains = []
+                for domain in subnet_data['Domain Names'].strip().split('\n'):
+                    domains.append(domain)
+                if len(domains) > 0:
+                    subnets[ip] = domains
+
+            for host in api.get_addresses(subnet_data['id']):
+                ip = host['ip']
+                if ip not in hosts:
+                    domains = [host['hostname'], ]
+                    if host['Domain Names']:
+                        for domain in host['Domain Names'].strip().split('\n'):
+                            domain.append(domain)
+                    hosts[ip] = domains
+        except APIException as e:
+            if e.code == 404:
+                pass
+            else:
+                raise e
+    for ip_address, domains in hosts.items():
+        ip = IP(ip_address)
+        ptr = ip.to_reverse()
+        for domain in domains:
+            if len(domain) > 0:
+                resources[section_title].append((ip, ptr, domain))
+
+    for subnet, domains in subnets.items():
+        append = True
+        net = Network(subnet)
+        for ip_address in hosts:
+            if IP(ip_address) in net:
+                append = False
+            if not append:
+                break
+        for subnet_2 in subnets:
+            net_2 = Network(subnet_2)
+            if net_2 in net and net_2.size() < net.size():
+                append = False
+            if not append:
+                break
+        if append:
+            prefix, length = net.to_tuple()
+            octets = int((length - length % 4) / 4)
+            prefix = prefix.replace(':', '')[:octets]
+            ptr = '*.{}.ip6.arpa'.format('.'.join(prefix[::-1]))
+            for domain in domains:
+                resources[section_title].append((net, ptr, domain))
+
+for zone, params in zones.items():
+    zone_network = params['prefix']
+    section = params['section']
+    template = Template(open(params['template_path']).read())
+    hash_file = var_dir + '/' + zone + '.hash'
+    sn_file = var_dir + '/' + zone + '.sn'
+    params['records'] = []
+    for network, record, value in resources[section]:
+        if network in zone_network:
+            params['records'].append(dict(resource=record, value=value))
+    zone_definition = template.render(**params)
+    zone_hash = sha256(zone_definition.encode('utf-8')).hexdigest()
+    try:
+        current_hash = open(hash_file).read()
+        sn = int(open(sn_file).read())
+    except IOError:
+        sn = int(params['initial_sn']) - 1
+        current_hash = ''
+    if current_hash != zone_hash:
+        sn += 1
+    zone_definition = zone_definition.replace('[ --- serial number --- ]', str(sn))
+    file = open(params['file_path'], 'w')
+    file.write(zone_definition)
+    file.close()
+    open(hash_file, 'w').write(zone_hash)
+    open(sn_file, 'w').write(str(sn))
